20b014e347f61edc759625ff40aa838ddef2b0d9
Erik <erikwjonsson@gmail.com>
2016-09-17 23:35:17 +0200

    Second commit. Frontedn design almost finished.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,62 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+
+$( document ).ready(function() {
+  $( ".fa-chevron-down" ).click(function() {
+    $(".hidden-info-box").slideUp( "slow", function() {
+      // Animation complete.
+    });
+     $(this).closest(".info-dropdown-box").children(".hidden-info-box").slideToggle( "slow", function() {
+      // Animation complete.
+    });
+	});
+
+$(".user-type-info-circle").hover(function(){
+    $(this).css("background-color", "#f2dede").children();
+    }, function(){
+    $(this).css("background-color", "#fff").children();
+});
+
+$(".user-type-info-circle").click(function() {
+  window.location = $(this).find("a").attr("href"); 
+  return false;
+});
+
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 0)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+})(jQuery); // End of use strict
+
+});
+
+
