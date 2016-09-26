b030b7fbe97687f008037e949e8d619f41ea7d34
Erik <erikwjonsson@gmail.com>
2016-09-26 19:24:23 +0200

    Fixed syntax error in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
     user_name:            'erikwjonsson@gmail.com',
     password:             'martius2',
     authentication:       'plain',
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
     :openssl_verify_mode  => 'none'
   }
   #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
