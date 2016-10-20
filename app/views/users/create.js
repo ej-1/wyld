88af9217db46368f861bdca7ceeb3dde00a309ae
Erik <erikwjonsson@gmail.com>
2016-10-21 00:52:34 +0200

    Fixed design error in partial when captchas fails. Changed error message for captcha.

--- app/views/users/create.js
+++ app/views/users/create.js
@@ -3,6 +3,12 @@ user_error = $("<%= j render(:partial => 'signup_page') %>");
 
 $('.signup-form').replaceWith(user_error);
 
+
+$(".field_with_errors .form-control").unwrap(); // Workaround to remove div messing up design when captcha fails.
+$(".field_with_errors .captcha-field").unwrap(); // Workaround to remove div messing up design when captcha fails.
+$(".field_with_errors .captcha-question-text").unwrap(); // Workaround to remove div messing up design when captcha fails.
+
+
 $('.error-body ').fadeIn( 700, function() {
 
  });
